81fa119526860bdf8522342c407ab92e5481ff23
mflood <hermannwest@gmail.com>
2025-06-28 12:11:29 -0600

    Blank iOS Project

---
+++ LanguageMirror/LanguageMirror/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LanguageMirror
+//
+//  Created by Matthew Flood on 6/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
